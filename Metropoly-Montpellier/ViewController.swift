68692620fe01f0233ced9914e8e7d567719b67c6
Mohamed Oulmaki <mohamed.oulmaki@epsi.fr>
2014-12-11 16:47:52 +0100

    Initial commit

---
+++ Metropoly-Montpellier/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Metropoly-Montpellier
+//
+//  Created by Mohamed on 25/11/2014.
+//  Copyright (c) 2014 cymodean.io. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var mapview: MKMapView!
+    var manager:CLLocationManager!
+    var myLocations: [CLLocation] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+        
+        mapview = MKMapView(frame: self.view.frame)
+        mapview.delegate = self
+        mapview.showsUserLocation = true
+        let template = "http://188.226.187.180:49169/mtp/{z}/{x}/{y}.png";
+        var overlay:MKTileOverlay = MKTileOverlay (URLTemplate: template)
+        overlay.canReplaceMapContent = true;
+        var level : MKOverlayLevel = MKOverlayLevel(rawValue: 1)!
+        mapview.addOverlay(overlay, level: level)
+        mapview.showsBuildings = false;
+        self.view.addSubview(mapview)
+        
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        if overlay is MKTileOverlay {
+            var rfo = MKTileOverlayRenderer (overlay: overlay)
+            return rfo
+        }
+        return nil
+    }
+    
+    
+}
+
+
